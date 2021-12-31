7432ee3357f8d64049c88257713197bad49c4755
will-johnston-goodyear <will_johnston@goodyear.com>
2021-12-31 10:20:08 -0500

    Adds MongoDB

--- constants.js
+++ constants.js
@@ -1,5 +1,10 @@
 const ErrorMessagesEnum = Object.freeze({
   DEFAULT: 'An error has occured',
+  USER_NOT_FOUND: 'No user found',
 });
 
-module.exports = ErrorMessagesEnum;
+const SuccessMessageEnum = Object.freeze({
+  USER_REMOVED: 'User deleted successfully',
+});
+
+module.exports = [ErrorMessagesEnum, SuccessMessageEnum];
--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,105 @@ const express = require('express');
 const router = express.Router();
 
 // Local Imports
-const users = require('../dummyUserDB');
-const ErrorMessagesEnum = require('../constants');
+const [ErrorMessagesEnum, SuccessMessageEnum] = require('../constants');
+const User = require('../models/user');
+
+router.post('/', async (req, res) => {
+  console.log(req.body);
+  try {
+    let user = new User(req.body);
+    user = await user.save();
+    res.status(200).json({
+      status: 200,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: error.message,
+    });
+  }
+});
 
 router.get('/all', async (req, res) => {
   try {
+    const users = await User.find();
     res.status(200).json({
+      status: 200,
       data: users,
     });
   } catch (error) {
     res.status(400).json({
-      message: ErrorMessagesEnum.DEFAULT, error,
+      status: 400,
+      data: users,
     });
   }
 });
 
 router.get('/:id', async (req, res) => {
-  let {id} = req.params;
-  id = Number(id);
   try {
-    const user = users.find((user) => user._id === id);
-    res.status(200).json({
-      data: user,
+    const user = await User.findOne({
+      _id: req.params.id,
+    });
+    if (user) {
+      res.status(200).json({
+        status: 200,
+        data: user,
+      });
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: ErrorMessagesEnum.USER_NOT_FOUND,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: error.message,
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (user) {
+      res.status(200).json({
+        status: 200,
+        data: user,
+      });
+    }
+    res.status(400).json({
+      status: 400,
+      message: ErrorMessagesEnum.USER_NOT_FOUND,
     });
   } catch (error) {
     res.status(400).json({
-      message: ErrorMessagesEnum.DEFAULT, error,
+      status: 400,
+      message: error.message,
+    });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndRemove(req.params.id);
+    if (user) {
+      res.status(200).json({
+        status: 200,
+        message: SuccessMessageEnum.USER_REMOVED,
+      });
+    }
+    res.status(400).json({
+      status: 400,
+      message: ErrorMessagesEnum.USER_NOT_FOUND,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: error.message,
     });
   }
 });

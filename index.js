926b91284ba99af9334ed54f09c2da954b58fa27
Will Johnston <wijohnst@gmail.com>
2021-12-31 08:03:48 -0500

    Stands up Express server (#3)
    Co-authored-by: will-johnston-goodyear <will_johnston@goodyear.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const { append } = require("express/lib/response");
 
-const post = preocess.env.PORT || 1112;
+const port = process.env.PORT || 1112;
 
 // Gets log details
 server.use(logger('dev'));
@@ -17,5 +17,9 @@ server.use(bodyParser.json());
 
 const usersRouter = require("./routes/users");
 server.use("/users", usersRouter);
+
+server.listen(port, () => {
+	console.log(`ThrivingPark is listening on port ${port}...`)
+})
 
 module.exports = server;

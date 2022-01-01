a4aeb3c88c837d5e04437315a91595d57f53afeb
will-johnston-goodyear <will_johnston@goodyear.com>
2021-12-31 20:57:54 -0500

    Updates user schema

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  _id: {type: String},
   profilePictureURL: {type: String},
 }, {collection: 'users'});
 
